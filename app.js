2e808dc3a6b3219b7dee70da08de2c1f0de2857f
Alex <57154874+awbrown404@users.noreply.github.com>
2020-04-07 00:28:11 -0400

    update 5

--- app.js
+++ app.js
@@ -1,37 +1,33 @@
-// Create function for data plotting
-function createPlots(id) {
-    //read data in with d3
-    d3.json("data/samples.json").then((data) => {
-        console.log(data)
+function displayGraphs(selectedID) {
+    d3.json("data/samples.json").then(function (data) {
+        // set data to variables
+        var metadata = data.metadata;
+        var samples = data.samples;
 
-        // washing frequency variable 
-        var wfreq = data.metadata.map(d => d.wfreq)
-        // console.log(wfreq)
+        // set filters to data variables to search for the necessary data
+        var metadataFilter = metadata.filter(result => result.id == selectedID);
+        var metadataResults = metadataFilter[0]
+        // console.log(metadataResults);
+        var samplesFilter = samples.filter(result => result.id == selectedID);
+        var samplesResults = samplesFilter[0]
+        // console.log(samplesResults);
 
-        // filter samples by id 
-        var samples = data.samples.filter(s => s.id.toString() === id)[0];
-        // console.log(samples)
+        // retrieve data
+        var sample_values = samplesResults.sample_values;
+        //console.log(sample_values);
+        var otu_ids = samplesResults.otu_ids;
+        // console.log(otu_ids);
+        var otu_labels = samplesResults.otu_labels;
+        // console.log(otu_labels);
 
-        // top 10
-        var sample_values = samples.sample_values.slice(0, 10);
-        // console.log(sample_values)
+        // build visuals
+        // horizontal bar graph
+        // isolate top 10 for bar graph
 
-        // top 10 otu ids for the plot otu 
-        var topTenOTU = samples.otu_ids.slice(0, 10).reverse();
-
-        var otu_ids = topTenOTU.map(d => "OTU " + d)
-
-        // console.log(otu_ids)
-
-        // get labels for top 10
-        var otu_labels = samples.otu_labels.slice(0, 10);
-        // console.log(otu_labels)
-
-        // set trace var
-        var trace = {
-            x: sample_values,
-            y: otu_ids,
-            text: otu_labels,
+        var barTrace = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(),
+            text: otu_labels.slice(0, 10).reverse(),
             marker: {
                 color: 'rgb(142,124,195)'
             },
@@ -39,11 +35,9 @@ function createPlots(id) {
             orientation: "h"
         };
 
-        // set data var
-        var data = [trace];
+        var barData = [barTrace];
 
-        // set layout var
-        var layout = {
+        var barLayout = {
             title: "Top 10 OTU",
             yaxis: {
                 tickmode: "linear"
@@ -56,30 +50,18 @@ function createPlots(id) {
             }
         };
 
-        // create plotly bar chart
-        Plotly.newPlot("bar", data, layout);
+        Plotly.newPlot("bar", barData, barLayout);
 
-        // Bubble Chart
+        // create bubblechart
         var bubbleTrace = {
             x: otu_ids,
-            y: sample_values.reverse(),
+            y: sample_values,
             text: otu_labels,
             mode: 'markers',
             marker: {
-                color: [
-                    '#1f77b4',
-                    '#ff7f0e',
-                    '#2ca02c',
-                    '#d62728',
-                    '#9467bd',
-                    '#8c564b',
-                    '#e377c2',
-                    '#7f7f7f',
-                    '#bcbd22',
-                    '#17becf'
-                ],
-                size: sample_values
-            },
+                size: sample_values,
+                color: otu_ids
+            }
         };
 
         var bubbleData = [bubbleTrace];
@@ -93,51 +75,69 @@ function createPlots(id) {
             width: 1000
         };
 
-        // plot bubble chart
         Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
-        var gaugeData = [{
-            domain: {
-                x: [0, 1],
-                y: [0, 1]
-            },
-            value: parseFloat(wfreq),
-            title: {
-                text: `Weekly Washing Frequncy`
-            },
+        // create gauge 
+        // gather data for washing frequency
+        var wfreq = metadataResults.wfreq;
+        console.log(wfreq)
+
+        var gaugeTrace = {
             type: "indicator",
+            value: wfreq,
+            title: {
+                text: "Belly Button Washing Frequency (Weekly)"
+            },
             mode: "gauge+number",
             gauge: {
                 axis: {
                     range: [null, 9]
                 },
                 steps: [{
-                        range: [0, 2],
-                        color: "yellow"
+                        range: [0, 1],
+                        color: "#ffffe4"
                     },
                     {
-                        range: [2, 4],
-                        color: "cyan"
+                        range: [1, 2],
+                        color: "#dff2c5"
                     },
                     {
-                        range: [4, 6],
-                        color: "teal"
+                        range: [2, 3],
+                        color: "#c7e3a8"
                     },
                     {
-                        range: [6, 8],
-                        color: "lime"
+                        range: [3, 4],
+                        color: "#b1d38d"
+                    },
+                    {
+                        range: [4, 5],
+                        color: "#9ec374"
+                    },
+                    {
+                        range: [5, 6],
+                        color: "#8cb25a"
+                    },
+                    {
+                        range: [6, 7],
+                        color: "#7aa241"
+                    },
+                    {
+                        range: [7, 8],
+                        color: "#6a9127"
                     },
                     {
                         range: [8, 9],
-                        color: "green"
-                    },
+                        color: "#5a8100"
+                    }
                 ]
             }
-        }];
+        };
+
+        var gaugeData = [gaugeTrace];
 
         var gaugeLayout = {
-            width: 700,
-            height: 600,
+            width: 500,
+            height: 400,
             margin: {
                 top: 20,
                 bottom: 40,
@@ -145,24 +145,18 @@ function createPlots(id) {
             }
         };
 
-        // plot gauge plot 
         Plotly.newPlot("gauge", gaugeData, gaugeLayout);
-
     });
-}
+};
 
-// create the function to get the necessary data
-function getInfo(id) {
-    // read the json file to get data
-    d3.json("Data/samples.json").then((data) => {
-
-        // get the metadata info for the demographic panel
+function displayDemographicInfo(selectedID) {
+    d3.json("data/samples.json").then(function (data) {
+        // set demographic info
+        // read in metadata
         var metadata = data.metadata;
 
-        console.log(metadata)
-
-        // filter meta data info by id
-        var result = metadata.filter(meta => meta.id.toString() === id)[0];
+        // filter metadata info by id
+        var results = metadata.filter(meta => meta.id.toString() === selectedID)[0];
 
         // select demographic panel to put data
         var demographicInfo = d3.select("#sample-metadata");
@@ -171,36 +165,34 @@ function getInfo(id) {
         demographicInfo.html("");
 
         // grab the necessary demographic data data for the id and append the info to the panel
-        Object.entries(result).forEach((key) => {
+        Object.entries(results).forEach((key) => {
             demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
         });
     });
-}
+};
 
-// create the function for the change event
-function optionChanged(id) {
-    createPlots(id);
-    getInfo(id);
-}
 
-// create the function for the initial data rendering
+function optionChanged(selectedID) {
+    displayGraphs(selectedID);
+    displayDemographicInfo(selectedID);
+};
+
 function init() {
-    // select dropdown menu 
     var dropdown = d3.select("#selDataset");
 
-    // read the data 
-    d3.json("Data/samples.json").then((data) => {
-        console.log(data)
+    d3.json("data/samples.json").then(function (data) {
+        console.log(data);
+        var ids = data.names;
 
-        // get the id data to the dropdwown menu
-        data.names.forEach(function (name) {
-            dropdown.append("option").text(name).property("value");
+        ids.forEach(id => {
+            dropdown.append("option").text(id).property("value", id)
         });
 
-        // call the functions to display the data and the plots to the page
-        createPlots(data.names[0]);
-        getInfo(data.names[0]);
+
+        // call functions to display the first result
+        displayGraphs(ids[0]);
+        displayDemographicInfo(ids[0]);
     });
-}
+};
 
 init();
